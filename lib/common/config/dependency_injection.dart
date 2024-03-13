a007a503f14a509fe99960e9c86a06bff19ca434
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2024-03-12 21:13:14 -0300

    feat: adicionado arquivo de controle de injeção de dependência

---
+++ lib/common/config/dependency_injection.dart
@@ -0,0 +1,9 @@
+import 'package:estacionamento_senhor_joao/common/theme/app_colors.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+mixin DependencyInjection {
+  static Future<void> init() async {
+    getIt.registerLazySingleton<AppColors>(() => AppColors());
+  }
+}
