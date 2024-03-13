0553957c02f6ce9de122a75a6feb7f2f372ccbbf
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2024-03-12 21:14:29 -0300

    faet: adicionado tela de splash

---
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.blue,
+    );
+  }
+}
