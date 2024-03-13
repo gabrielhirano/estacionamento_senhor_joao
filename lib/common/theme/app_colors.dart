30389ed49b7381ecf742b1169a2ad02e06a43988
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2024-03-12 21:13:58 -0300

    feat: adicionado arquivo para agrupamento de cores

---
+++ lib/common/theme/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Brands - Primary
+  final colorBrandPrimaryBlue = const Color(0xFF4974E5);
+  final colorBrandPrimaryBlueLight = const Color(0xFF017BC8);
+
+  // Generics
+  final yellow = const Color(0xFFDEFF00);
+  final white = const Color(0xFFFFFFFF);
+  final grey = const Color(0xFF757575);
+  final green = const Color(0xFF008A00);
+  final red = const Color(0xFFDC3535);
+  final purple = const Color(0xFFA479C3);
+  final orange = const Color(0xFFF45109);
+  final transparent = Colors.transparent;
+
+  // Generics - Dark
+  final greyDark = const Color(0xFF4A4A4A);
+  final blueGreyDark = const Color(0xFF617185);
+}
---
+++ lib/common/theme/theme_global.dart
@@ -0,0 +1,5 @@
+import 'package:estacionamento_senhor_joao/common/config/dependency_injection.dart';
+
+import 'app_colors.dart';
+
+final appColors = getIt<AppColors>();
